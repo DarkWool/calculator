4112bf4142747b3ad5b861d9ccd7373a76ca6df0
DarkWool <cooper27@live.com.mx>
2021-11-06 19:52:59 -0600

    Added event listeners for buttons and created functions to display the numbers, delete one character at a time and restart the calculator.

--- static/functionality.js
+++ static/functionality.js
@@ -1,3 +1,45 @@
+const NUMBERS = document.body.getElementsByClassName('number');
+const OPERATORS = document.body.getElementsByClassName('operator');
+const AC = document.getElementById('ac');
+const DEL = document.getElementById('del');
+const ENTER = document.getElementById('enter-btn');
+const lastOp = document.getElementById('lastOperation');
+const actualOp = document.getElementById('currentOperation');
+
+AC.addEventListener('click', restartCalculator);
+DEL.addEventListener('click', deleteOneChar);
+
+for (let button of NUMBERS) {
+    button.addEventListener('click', displayNumber);
+}
+
+for (let button of OPERATORS) {
+    button.addEventListener('click', operator);
+}
+
+
+function operator(e) {
+    lastOp.textContent += `${e.target.textContent}`;
+}
+
+function displayNumber(e) {
+    actualOp.textContent += `${e.target.textContent}`;
+}
+
+function restartCalculator() {
+    lastOp.textContent = '';
+    actualOp.textContent = '0';
+}
+
+function deleteOneChar() {
+    actualOp.textContent = actualOp.textContent.slice(0, actualOp.textContent.length - 1);
+}
+
+
+
+
+
+
 // Calculator functions.
 function add(a, b) {
     return a + b;
