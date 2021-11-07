e6027c5899383213ce0233a3b6704f4a6e5821a6
DarkWool <cooper27@live.com.mx>
2021-11-06 20:58:36 -0600

    Made the calculator work through buttons, added functionality for the operators function and fixed an error where the first number was always zero, not its replaced by the first number entered by the user.

--- static/functionality.js
+++ static/functionality.js
@@ -6,6 +6,10 @@ const ENTER = document.getElementById('enter-btn');
 const lastOp = document.getElementById('lastOperation');
 const actualOp = document.getElementById('currentOperation');
 
+let firstNum;
+let secondNum;
+let operator;
+
 AC.addEventListener('click', restartCalculator);
 DEL.addEventListener('click', deleteOneChar);
 
@@ -14,19 +18,36 @@ for (let button of NUMBERS) {
 }
 
 for (let button of OPERATORS) {
-    button.addEventListener('click', operator);
+    button.addEventListener('click', operatorBtn);
 }
 
 
-function operator(e) {
-    lastOp.textContent += `${e.target.textContent}`;
+function operatorBtn(e) {
+    lastOp.textContent = `${actualOp.textContent} ${e.target.textContent}`;
+    
+    if (firstNum) {
+        secondNum = actualOp.textContent;
+        firstNum = operate(operator, +firstNum, +secondNum);
+
+        operator = e.target.dataset.key;
+        lastOp.textContent = `${firstNum} ${operator}`;
+    } else {
+        firstNum = actualOp.textContent;
+        operator = e.target.dataset.key;
+    }
+    actualOp.textContent = '0';
 }
 
 function displayNumber(e) {
-    actualOp.textContent += `${e.target.textContent}`;
+    (actualOp.textContent === '0') ? actualOp.textContent = e.target.textContent :
+        actualOp.textContent += `${e.target.textContent}`; 
 }
 
 function restartCalculator() {
+    firstNum = 0;
+    secondNum = 0;
+    operator = '';
+
     lastOp.textContent = '';
     actualOp.textContent = '0';
 }
